58ec9483b177a7364713bb9930a16334e2b474ba
xu <xu@Macmini.local>
2013-11-03 14:46:22 +0800

    Initial Commit

---
+++ Turkey/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Turkey
+//
+//  Created by xu on 13-11-3.
+//  Copyright (c) 2013年 xu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
